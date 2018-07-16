e9ed63156edb5dbb54d48c26f689385404bfbe3c
yinm <yinm.cminor9@gmail.com>
2018-07-16 14:22:29 +0900

    Check Vuex's module mode

---
+++ nuxt-project/store/user.js
@@ -0,0 +1,17 @@
+import Vuex from 'vuex'
+
+export const state = () => ({
+  list: []
+})
+
+export const mutations = {
+  addUser(state, user) {
+    state.list.push(user)
+  }
+}
+
+export const actions = {
+  addUser({ commit }, { user }) {
+    commit('addUser', user)
+  }
+}
