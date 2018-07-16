f84f87a70385313ae21dd12e2fedb9d8a1b70eb5
yinm <yinm.cminor9@gmail.com>
2018-07-16 14:46:33 +0900

    Check error page

---
+++ nuxt-project/layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <template v-if="isNotFound">
+      <h1>404 not found</h1>
+      <nuxt-link to="/">Back to home</nuxt-link>
+    </template>
+    <template v-else>
+      <h1>Error</h1>
+      <nuxt-link to="/">Back to home</nuxt-link>
+    </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    computed: {
+      isNotFound() {
+        return this.error.statusCode === 404
+      }
+    }
+  }
+</script>
